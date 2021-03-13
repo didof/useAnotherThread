85d7a19b78840afe3f829867a39f6ee4b0014ba3
didof <didonato.fr@gmail.com>
2021-03-13 13:18:45 +0100

    fix typo

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -12,7 +12,7 @@ const Alert = () => {
     <div className='notification is-warning' ref={notificationRef}>
       <button className='delete' onClick={onHideHandler}></button>
       You are trying to run a number of iterations directly on the{' '}
-      <b>single-thread</b>. This, depending on your devise, could lead to
+      <b>single-thread</b>. This, depending on your device, could lead to
       browser <i>freezing</i> or even <i>crashing</i>.
     </div>
   )
